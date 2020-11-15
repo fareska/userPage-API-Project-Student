69b77b7f5fbf38835692b59c38305dac82c927c2
fareska <fares.xb@gmail.com>
2020-11-16 00:36:10 +0200

    First version

--- APIManager.js
+++ APIManager.js
@@ -1,7 +1,65 @@
-//This is the class that will manage all your APIs
-
 class APIManager {
-    constructor() {
-        this.data = {}
-    }
+  constructor() {
+    this.data = {};
+  }
+
+  getUserInfo() {
+    $.ajax({
+      url: "https://randomuser.me/api/?results=7",
+      dataType: "json",
+      async: false,
+      success: (data) => {
+        this.data.name = data.results.map(
+          (n) => n.name.first + " " + n.name.last
+        )[0];
+        this.data.address = data.results.map(
+          (l) => l.location.city + ", " + l.location.state
+        )[0];
+        this.data.picture = data.results.map((p) => p.picture.medium)[0];
+        this.data.friends = data.results
+          .map((f) => f.name.first + " " + f.name.last)
+          .splice(1);
+      },
+    });
+  }
+
+  getQuote() {
+    $.ajax({
+      url: "https://api.kanye.rest",
+      dataType: "json",
+      async: false,
+      success: (data) => (this.data.quote = data),
+    });
+  }
+
+  getPoke() {
+    $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon/25",
+      dataType: "json",
+      async: false,
+      success: (data) => {
+        this.data.pokeName = data.name;
+        this.data.pokePicture = data.sprites.other.dream_world.front_default;
+      },
+    });
+  }
+
+  getText() {
+    $.ajax({
+      url:
+        "https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1",
+      dataType: "json",
+      async: false,
+      success: (data) => (this.data.lipsum = data),
+    });
+  }
+
+  getData = () => this.data;
+
+  loadData = () => {
+    this.getUserInfo();
+    this.getQuote();
+    this.getPoke();
+    this.getText();
+  };
 }
--- Renderer.js
+++ Renderer.js
@@ -1,4 +1,63 @@
 
 class Renderer {
 
+    loadInfo(data) {
+        const source = $("#details-template").html()
+        const template = Handlebars.compile(source)
+
+        // const pName = template({name:data.name})
+        // const pAddress = template({address:data.address})
+        // const pPicture = template({picture:data.picture})
+       
+        const newHtml = template({name:data.name, address:data.address,picture:data.picture})
+        $(".user-container").append(newHtml)
+        // $(".user-container").append(pAddress)
+        // $(".user-container").append(pPicture)
+    }
+
+    loadFriends(friends){
+        const source = $("#friends-template").html()
+        const template = Handlebars.compile(source)
+
+        for (let friend of friends){
+            const dude = template({dude:friend})
+            $(".friends-container").append(dude)
+        }
+    }
+
+    loadQuote(data){
+        const source = $("#quote-template").html()
+        const template = Handlebars.compile(source)
+        const newHtml = template({link:data.quote.quote})
+        $(".quote-container").append(newHtml)
+    }
+
+    loadPoke(data){
+        const source = $("#poke-template").html()
+        const template = Handlebars.compile(source)
+
+        const pokeHtml = template({pic:data.pokePicture})
+        const pokeHtml2 = template({name:data.pokeName})
+        $(".pokemon-container").append(pokeHtml)
+        $(".pokemon-container").append(pokeHtml2)
+    }
+
+    loadText(data){
+        const source = $("#text-template").html()
+        const template = Handlebars.compile(source)
+
+        const textHtml=template({text:data.lipsum})
+        $(".meat-container").append(textHtml)
+    }
+
+
+    Render(data){
+        this.loadInfo(data)
+        this.loadFriends(data.friends)
+        this.loadQuote(data)
+        this.loadPoke(data)
+        this.loadText(data)
+    }
 }
+
+
